fc47c885568fac81648d27fad6d555fc1fddf1d5
Nikita <nikitapavlov2828@gmail.com>
2022-01-19 17:14:56 +0300

    add connect to dbus

--- spower-control.cpp
+++ spower-control.cpp
@@ -1,7 +1,17 @@
+#include <boost/asio/io_service.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+
 #include <iostream>
+#include <memory>
 
 int main()
 {
-	std::clog << "spower-control\n";
+	boost::asio::io_service io;
+	auto conn = std::make_shared<sdbusplus::asio::connection>(io);
+	
+    conn->request_name("xyz.openbmc_project.State.Host");
+    conn->request_name("xyz.openbmc_project.State.Chassis");
+    conn->request_name("xyz.openbmc_project.Chassis.Buttons");
+    
 	return 0;
 }
